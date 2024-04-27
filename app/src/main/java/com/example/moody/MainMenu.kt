846cd7cbf54002ac9171cf093795e060e013df53
Suchindu Malith <122116912+Suchindu@users.noreply.github.com>
2024-04-27 18:33:01 +0530

    successful progress

--- app/src/main/java/com/example/moody/MainMenu.kt
+++ app/src/main/java/com/example/moody/MainMenu.kt
@@ -1,20 +1,30 @@
 package com.example.moody
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+//import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+        val startbutton: Button = findViewById(R.id.button)
+
+        startbutton.setOnClickListener {
+            // Create an Intent to start MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            // Start the MainActivity
+            startActivity(intent)
         }
+        }
+
     }
-}
